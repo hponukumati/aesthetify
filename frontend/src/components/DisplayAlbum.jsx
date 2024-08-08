d7c5565ffd2577d8818cdbd719806f7542cd49c4
Sri Harsha Ponukumati <harshaponukumati@Harshas-MacBook.attlocal.net>
2024-08-07 23:03:45 -0700

    updated displayalbum.jsx

--- frontend/src/components/DisplayAlbum.jsx
+++ frontend/src/components/DisplayAlbum.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData } from '../assets/assets';
+import { albumsData, assets } from '../assets/assets';
 
 export const DisplayAlbum = () => {
 
     const {id}= useParams();
     const albumData=albumsData[id];
+    
 
   return (
     <>
         <Navbar/>
+        <div className='mt-10 flex gap-9 flex-col md:flex-row md:items-end'>
+          <img className='w-48 rounded'src={albumData.image} alt="" />
+          <div className='flex flex-col'>
+            <p>Playlist</p>
+            <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+            <h4>{albumData.desc}</h4>
+            <p className='mt-1'>
+              <img className='inline-block w-5' src={assets.spotify_logo} alt=''/>
+              <b>Spotify</b>
+               | 2 Likes |
+              <b> 50 songs,</b>
+               about 2hr 30 min
+            </p>
+          </div>
+        </div>
     </>
   )
 }
